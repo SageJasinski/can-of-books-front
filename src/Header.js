f8c671ae277fb3c83931f200c47a8d530baa3d33
Sage Jasinski <sagejasinski@gmail.com>
2022-10-05 18:56:28 +0000

    form hidding added

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import './Styles/Navbar.scss';
+import Logo from './img/Logo2.png'
 
 class Header extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" >
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
+        <Navbar.Brand>
+            <img 
+            alt='' 
+            src={Logo}  
+            width="200px"
+            height="50px" 
+            className="d-inline-block align-top"/>
+        </Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
         <NavItem><Link to="/about" className='nav-link'>About</Link></NavItem>
       </Navbar>
